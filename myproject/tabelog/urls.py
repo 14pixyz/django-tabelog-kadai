25f6d595c84ec8f60cc2af16b674201020eeb53b
14pixyz <ryo.pixy@gmail.com>
2024-05-16 06:34:24 +0900

    ログアウトリンクの修正

--- myproject/tabelog/urls.py
+++ myproject/tabelog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from allauth.account.views import LogoutView    #allauthの中にあるLogoutViewを持ってくる
 from . import views
 
 app_name = 'tabelog'
@@ -6,4 +7,5 @@ app_name = 'tabelog'
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('welcome/', views.WelcomeView.as_view(), name='welcome'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
