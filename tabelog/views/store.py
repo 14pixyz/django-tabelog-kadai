f2e2f10a89adaaa3ae048ccd41f9e15b96a8c26a
14pixyz <ryo.pixy@gmail.com>
2024-05-26 14:07:38 +0900

    カテゴリ検索の追加、トップページからの画面検索を追加

--- tabelog/views/store.py
+++ tabelog/views/store.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from tabelog.models import Store
+from tabelog.models import Store, Category
 
 
 class StoreListView(ListView):
@@ -11,12 +11,18 @@ class StoreListView(ListView):
         queryset = super().get_queryset(**kwargs)
         query = self.request.GET
 
+        # トップ画面からの検索
+        if top_query := query.get('top_query'):
+            queryset = queryset.filter(name__icontains=top_query).order_by('id')
         # フィルタリング
-        if q := query.get('q'):
-            queryset = queryset.filter(name__icontains=q).order_by('budget')
+        if store_name := query.get('store_name'):
+            queryset = queryset.filter(name__icontains=store_name).order_by('id')
         # ソート
         if sort_by := query.get('sort_by'):
             queryset = queryset.order_by(sort_by)
+        # カテゴリ
+        if category := query.get('category'):
+            queryset = queryset.filter(category__exact=category)
 
         return queryset
 
@@ -24,6 +30,8 @@ class StoreListView(ListView):
         context = super().get_context_data(**kwargs)
         context['sort_by'] = self.request.GET.get('sort_by', '')
         context['q'] = self.request.GET.get('q', '')
+        context['select_category'] = self.request.GET.get('category')
+        context['categorys'] = Category.objects.all()
         return context
 
 
